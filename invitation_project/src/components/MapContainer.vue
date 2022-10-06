9119fadc7aa6cc85222bc9604803a98da426c970
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-06 23:59:51 +0200

    lint

--- invitation_project/src/components/MapContainer.vue
+++ invitation_project/src/components/MapContainer.vue
@@ -13,22 +13,22 @@ export default {
        map,
     }
   },
-methods:{
-  initMap(){
+  methods:{
+    initMap(){
       AMapLoader.load({
-          key:"4742216123f6ad9602207f138dea916f",             // 申请好的Web端开发者Key，首次调用 load 时必填
-          version:"2.0",      // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-          plugins:[''],       // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+        key:"4742216123f6ad9602207f138dea916f",             // 申请好的Web端开发者Key，首次调用 load 时必填
+        version:"2.0",      // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        plugins:[''],       // 需要使用的的插件列表，如比例尺'AMap.Scale'等
       }).then((AMap)=>{
           this.map = new AMap.Map("container",{  //设置地图容器id
-              viewMode:"3D",    //是否为3D地图模式
-              zoom:5,           //初始化地图级别
-              center:[22.53030,113.93031], //初始化地图中心点位置
+            viewMode:"3D",    //是否为3D地图模式
+            zoom:5,           //初始化地图级别
+            center:[22.53030,113.93031], //初始化地图中心点位置
           });
       }).catch(e=>{
-          console.log(e);
+        console.log(e);
       })
-    },
+      },
   },
   mounted(){
     //DOM初始化完成进行地图初始化
@@ -38,10 +38,10 @@ methods:{
 </script>
 
 <style  scoped>
-  #container{
-      padding:0px;
-      margin: 0px;
-      width: 100%;
-      height: 800px;
-  }
+#container{
+  padding:0px;
+  margin: 0px;
+  width: 60%;
+  height: 800px;
+}
 </style>
