f44ca0ee2098e6aa8bf66b022949dbae8c68a58f
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-10 20:14:06 +0200

    add 2 new views

---
+++ invitation_project/src/views/AddInvitation.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    Use this page to add a new invitation into the database
+</div>
+</template>
+
+<script>
+</script>
---
+++ invitation_project/src/views/AllInvitation.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  This page shows all current invitations in time
+</div>
+</template>
+
+<script>
+</script>
