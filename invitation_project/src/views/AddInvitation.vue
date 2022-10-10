f25f345eec40e18f7f4074aba1f2112679ad4dcd
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-10 21:34:54 +0200

    Update AddInvitation.vue

--- invitation_project/src/views/AddInvitation.vue
+++ invitation_project/src/views/AddInvitation.vue
@@ -1,8 +1,45 @@
 <template>
-<div>
-    Use this page to add a new invitation into the database
+<div id="inputarea" class="pad">
+    <span>姓名：</span> <br/>
+    <span></span>
+    <input v-model="customerName"><br/> <br/>
+    <span>日期：</span>
+    <Datepicker v-model="date"></Datepicker> 
 </div>
 </template>
 
 <script>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import { ref, onMounted } from 'vue';
+export default {
+    name: 'SubmitInvitation',
+    components: { Datepicker },
+    setup() {
+        const date = ref();
+        // For demo purposes assign range from the current date
+        onMounted(() => {
+            const startDate = new Date();
+            const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
+            date.value = [startDate, endDate];
+        })
+        return {
+            date,
+        }
+    }
+}
 </script>
+
+<style scoped>
+#inputarea {
+    text-align: left;
+    line-height: 1.4em;
+    padding: 10px;
+    margin:20px
+}
+.pad {
+    padding: 10px;
+    margin:20px;
+    width: 80%;
+}
+</style>
