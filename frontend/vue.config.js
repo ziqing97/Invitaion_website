f6165752cdda0f5631d82c30a9c2454b7f285808
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-16 01:30:11 +0200

    Revert "add package"
    This reverts commit 94159fcff8f8b6b2bbc16304abe693972e328360.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
-  devServer: {
-    proxy: "http://47.100.131.89:80",
-  },
-  configureWebpack: {
-    devtool: 'source-map'
-  },
-  publicPath:'./'
 })
