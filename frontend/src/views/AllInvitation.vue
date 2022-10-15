fcf993ed187c1f25d0c04360d94f258cb246bfe3
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-16 01:35:50 +0200

    Update AllInvitation.vue

--- frontend/src/views/AllInvitation.vue
+++ frontend/src/views/AllInvitation.vue
@@ -1,8 +1,26 @@
 <template>
-<div>
-  This page shows all current invitations in time
-</div>
-</template>
-
-<script>
-</script>
+  <div>
+    <li id="all_inv_list" v-for="item in AllInvitation" v-bind:key="item">
+      <router-link :to="{ name: 'InvitationRoute', params: { invitation_name: item }}">{{item}}</router-link>
+    </li>
+    
+  </div>
+  </template>
+  
+  <script>
+  // import axios from 'axios'
+  export default {
+    name: 'AllInvitation',
+    data(){
+      return{
+        AllInvitation:['yuziqing','liuliyi','longcheng']
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .all_inv_list{
+    text-align: left;
+  }
+  </style>
