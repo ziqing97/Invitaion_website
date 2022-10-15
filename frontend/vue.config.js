19f6a7f45f7f774435412adc025a32ca5dcf7640
ziqing97 <ziqing.yu0623@gmail.com>
2022-10-16 01:16:33 +0200

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  devServer: {
+    proxy: "http://47.100.131.89:80",
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  publicPath:'./'
 })
